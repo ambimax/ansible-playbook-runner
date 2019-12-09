b77adf1f10a919dc5e060bc5cbe16e406d21baae
wysselbie <wissla1993@aol.com>
2019-12-09 12:46:06 +0100

    bugfix ssh_key conditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,11 @@ if [ -f "${KNOWN_HOSTS_FILE}" ]; then
     cp ${KNOWN_HOSTS_FILE} /root/.ssh/known_hosts;
 fi;
 
-if [ -z "${SSH_KEY}" ]; then
-    echo "Yes, I am in!"
-    ssh-agent -a ${SSH_KEY} > /dev/null
-    ssh-add - <<< "${SSH_KEY}"
-    # echo ${SSH_KEY} > /root/.ssh/id_rsa;
+if [ ! -z "${SSH_KEY}" ]; then
+    echo ${SSH_KEY} > /root/.ssh/id_rsa;
 fi;
 
 ls -lah /root/.ssh
-echo "ssh_key"
-echo ${SSH_KEY}
-echo "\n"
 
 if [ -f "${REQUIRED_ROLES_FILE}" ]; then
     ansible-galaxy install -r ${REQUIRED_ROLES_FILE} -p roles;
